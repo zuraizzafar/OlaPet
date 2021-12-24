60dedca03c82048898af42bbb8437922282b35b8
Muhammad Zuraiz Zafar <zuraiz49@gmail.com>
2021-12-24 19:56:34 +0500

    Changing BRowsersyn configuration

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,10 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps();
-    
-mix.browserSync('http://localhost:8000/');
+    .sourceMaps()
+    .browserSync({
+        proxy: 'app:80',
+        open: false,
+        port: 8081
+    })
+    ;
